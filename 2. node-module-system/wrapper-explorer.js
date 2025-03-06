13b301481c15049cf62dc7dcb26fac292d63444c
Ma5Codes <117619833+Ma5Codes@users.noreply.github.com>
2025-03-06 21:34:53 +0300

    Start of learning basics!!

---
+++ 2. node-module-system/wrapper-explorer.js
@@ -0,0 +1,9 @@
+
+console.log("Node module wrapper explorer");
+
+console.log("__filename in wrapper explorer", __filename);
+console.log("__dirname in wrapper explorer", __dirname);
+
+module.exports.greet = function (name) {
+  console.log(`Hello ${name}`);
+};
---
+++ 2. node-module-system/wrapper-in.js
@@ -0,0 +1,8 @@
+const wrapperExplorer = require("./wrapper-explorer");
+console.log("in wrapper-in.js");
+
+console.log("__filename in wrapper-in", __filename);
+console.log("__dirname in wrapper-in", __dirname);
+
+wrapperExplorer.greet("ma5Codes in wrapper-in");
+
